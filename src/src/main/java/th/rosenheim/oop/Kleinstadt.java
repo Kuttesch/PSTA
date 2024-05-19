64b8f19f26d8b60f131f19fd412a6cd762a09b16
Kuttesch <106610385+Kuttesch@users.noreply.github.com>
2024-05-19 13:21:28 +0200

    added Kleinstadt class

--- src/src/main/java/th/rosenheim/oop/Kleinstadt.java
+++ src/src/main/java/th/rosenheim/oop/Kleinstadt.java
@@ -1,1 +1,17 @@
 package th.rosenheim.oop;
+
+public class Kleinstadt extends Stadt {
+    public Kleinstadt(String name, Wetter wetter) {
+        super(name, wetter);
+    }
+
+    @Override
+    public String getContent() {
+        return "<p>In " + name + " ist es " + wetter.toString().toLowerCase() + ".</p>";
+    }
+
+    @Override
+    public String getURL() {
+        return "wetter_kleinstadt_" + formatNameForURL() + ".html";
+    }
+}
