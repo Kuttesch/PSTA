32b790ea29e2c3a8d45daf7295c7954dbcf87195
Kuttesch <106610385+Kuttesch@users.noreply.github.com>
2024-05-19 17:10:06 +0200

    added JDoc-Comments to WebsitenGenerator.java

--- src/src/main/java/th/rosenheim/oop/WebsitenGenerator.java
+++ src/src/main/java/th/rosenheim/oop/WebsitenGenerator.java
@@ -5,13 +5,28 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * The WebsitenGenerator class is responsible for generating the weather website including the navigation and Subsites.
+ * It allows adding cities and generating Subsites for each city.
+ */
 public class WebsitenGenerator {
     private List<Stadt> staedte = new ArrayList<>();
 
+    /**
+     * Adds a city to the list of cities.
+     *
+     * @param stadt The city to add.
+     */
     public void addStadt(Stadt stadt) {
         staedte.add(stadt);
     }
 
+    /**
+     * Generates a navigation string that contains links to the Subsite of all the cities added.
+     * The links are separated by a pipe character (|).
+     *
+     * @return The generated navigation string.
+     */
     public String getNavigation() {
         String staedteString = String.join("| ", staedte.stream()
                 .map(stadt -> "<a href=\"" + stadt.getURL() + "\">" + stadt.name + "</a>")
@@ -20,10 +35,23 @@ public class WebsitenGenerator {
 
     }
 
+    /**
+     * Generates the HTML code for a web page. The page contains the provided navigation and content.
+     *
+     * @param navigation    The navigation to include in the page.
+     * @param content       The content to include in the page.
+     * @return              The generated HTML code.
+     */
     public String generatePage(String navigation, String content) {
         return "<html><body>" + navigation + content + "</body></html>";
     }
 
+    /**
+     * Generates the web pages for all the cities added. It first generates an index page that contains links to all the city pages.
+     * Then, it generates a web page for each city. The city page contains the same navigation as the index page and the content of the city.
+     * 
+     * @throws IOException if an error occurs while writing the files
+     */
     public void generate() {
         String navigation = getNavigation();
         try {
