781a1d51bb946e22bfde2e298be59b6f7c8d541c
Kuttesch <106610385+Kuttesch@users.noreply.github.com>
2024-05-19 13:36:12 +0200

    added Grossstadt class (ß -> ss)

--- src/src/main/java/th/rosenheim/oop/Großstadt.java
+++ src/src/main/java/th/rosenheim/oop/Großstadt.java
@@ -1,1 +1,24 @@
 package th.rosenheim.oop;
+import java.util.List;
+
+public class Grossstadt extends Stadt {
+    private List<String> stadtteile;
+
+    public Grossstadt(String name, Wetter wetter, List<String> stadtteile) {
+        super(name, wetter);
+        this.stadtteile = stadtteile;
+    }
+
+    @Override
+    public String getContent() {
+        String stadtteileString = String.join(", ", stadtteile.subList(0, stadtteile.size() - 1))
+                + " und " + stadtteile.get(stadtteile.size() - 1);
+        return "<p>In " + name + " ist es " + wetter.toString().toLowerCase() + ".</p>"
+                + "<p> Dies trifft auch für " + stadtteileString + "zu.</p>";
+    }
+
+    @Override
+    public String getURL() {
+        return "wetter_grossstadt_" + formatNameForURL() + ".html";
+    }
+}
