5c0c5dca148ed4408b7ec8b69f8c41a69506c788
Kuttesch <106610385+Kuttesch@users.noreply.github.com>
2024-05-19 17:09:22 +0200

    added JDoc-Comments to App.java

--- src/src/main/java/th/rosenheim/oop/App.java
+++ src/src/main/java/th/rosenheim/oop/App.java
@@ -3,7 +3,17 @@ package th.rosenheim.oop;
 import java.io.IOException;
 import java.util.List;
 
+/**
+ * The main class of the application.
+ */
 public class App {
+    /**
+     * The main method of the application.
+     * It creates an instance of the WebsitenGenerator, creates example instances of Kleinstadt and Grossstadt,
+     * It adds the example cities to the generator, and generates the example website.
+     *
+     * @param args Possible command-line arguments.
+     */
     public static void main(String[] args) {
         WebsitenGenerator generator = new WebsitenGenerator();
 
