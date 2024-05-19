a9a3a3f49c29627c29bb6c7de2e4264e545a63ae
Kuttesch <106610385+Kuttesch@users.noreply.github.com>
2024-05-19 17:09:54 +0200

    added JDoc-Comments to Stadt.java

--- src/src/main/java/th/rosenheim/oop/Stadt.java
+++ src/src/main/java/th/rosenheim/oop/Stadt.java
@@ -1,14 +1,28 @@
 package th.rosenheim.oop;
 
+/**
+ * The abstract class representing a city.
+ */
 public abstract class Stadt {
     protected String name;
     protected Wetter wetter;
 
+    /**
+     * Constructs a Stadt object with the specified name and weather.
+     *
+     * @param name   the name of the city
+     * @param wetter the weather of the city
+     */
     public Stadt(String name, Wetter wetter) {
         this.name = name;
         this.wetter = wetter;
     }
 
+    /**
+     * Formats the name of the city for use in a URL.
+     *
+     * @return the formatted name of the city
+     */
     protected String formatNameForURL() {
         return name.toLowerCase()
                     .replace("ä", "ae")
@@ -17,6 +31,18 @@ public abstract class Stadt {
                     .replace("ß", "ss")
                     .replace(" ", "_");
     }
+
+    /**
+     * Gets the content of the city.
+     *
+     * @return the content of the city's Subsite
+     */
     public abstract String getContent();
+
+    /**
+     * Gets the URL of the city.
+     *
+     * @return the URL of the city's Subsite
+     */
     public abstract String getURL();
 }
