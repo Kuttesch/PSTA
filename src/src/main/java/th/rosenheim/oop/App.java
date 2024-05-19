83e5be4899f98921722e7d3cf5bdf45aba5383f3
Kuttesch <106610385+Kuttesch@users.noreply.github.com>
2024-05-19 13:08:40 +0200

    created necessary class files

--- src/src/main/java/th/rosenheim/oop/App.java
+++ src/src/main/java/th/rosenheim/oop/App.java
@@ -1,17 +1,1 @@
 package th.rosenheim.oop;
-
-
-public class App {
-
-
-    public static void main(String[] args) {
-        init();
-    }
-
-
-
-
-    public static void init() {
-        System.out.println("Hello World!");
-    }
-}
