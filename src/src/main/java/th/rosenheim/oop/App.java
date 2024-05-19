3bb2c8e0ab1a419a1bcacc7f9bb579adc2c38cb8
Kuttesch <106610385+Kuttesch@users.noreply.github.com>
2024-05-19 14:28:32 +0200

    added App.java class

--- src/src/main/java/th/rosenheim/oop/App.java
+++ src/src/main/java/th/rosenheim/oop/App.java
@@ -1,1 +1,20 @@
 package th.rosenheim.oop;
+
+import java.io.IOException;
+import java.util.List;
+
+public class App {
+    public static void main(String[] args) {
+        WebsitenGenerator generator = new WebsitenGenerator();
+
+        Kleinstadt rosenheim = new Kleinstadt("Rosenheim", Wetter.WOLKIG);
+        Grossstadt muenchen = new Grossstadt("München", Wetter.REGNERISCH, List.of("Schwabing", "Sendling", "Lehel", "Nymphenburg"));
+        Grossstadt koeln = new Grossstadt("Köln", Wetter.SONNIG, List.of("Raderthal", "Ehrenfeld", "Nippes", "Poll", "Esch", "Pesch", "Kalk"));
+
+        generator.addStadt(rosenheim);
+        generator.addStadt(muenchen);
+        generator.addStadt(koeln);
+
+        generator.generate();
+    }
+}
