950ef20a3bcd10f0ce2a60d60f921b61a075886f
Kuttesch <106610385+Kuttesch@users.noreply.github.com>
2024-05-19 13:15:16 +0200

    added Stadt class

--- src/src/main/java/th/rosenheim/oop/Stadt.java
+++ src/src/main/java/th/rosenheim/oop/Stadt.java
@@ -1,1 +1,22 @@
 package th.rosenheim.oop;
+
+public abstract class Stadt {
+    protected String name;
+    protected Wetter wetter;
+
+    public Stadt(String name, Wetter wetter) {
+        this.name = name;
+        this.wetter = wetter;
+    }
+
+    protected String formatNameForURL() {
+        return name.toLowerCase()
+                    .replace("ä", "ae")
+                    .replace("ö", "oe")
+                    .replace("ü", "ue")
+                    .replace("ß", "ss")
+                    .replace(" ", "_");
+    }
+    public abstract String getContent();
+    public abstract String getURL();
+}
