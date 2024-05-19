097fd45a621d6b7be1684e570a29e807d3deb3d9
Kuttesch <106610385+Kuttesch@users.noreply.github.com>
2024-05-19 17:10:09 +0200

    added JDoc-Comments to Wetter.java

--- src/src/main/java/th/rosenheim/oop/Wetter.java
+++ src/src/main/java/th/rosenheim/oop/Wetter.java
@@ -1,5 +1,21 @@
 package th.rosenheim.oop;
+/**
+ * The Wetter enum represents the different weather conditions used in the WebsiteGenerator.
+ * It has three values: SONNIG, WOLKIG and REGNERISCH.
+ * It is used throughout the project to represent the weather in a city.
+ * **/
 
 public enum Wetter {
-    SONNIG, WOLKIG, REGNERISCH;
+    /**
+     * The SONNIG value represents a sunny weather condition.
+     * **/
+    SONNIG,
+    /**
+     * The WOLKIG value represents a cloudy weather condition.
+     * **/
+    WOLKIG,
+    /**
+     * The REGNERISCH value represents a rainy weather condition.
+     * **/
+    REGNERISCH;
 }
